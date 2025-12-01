93c09d82049d480507c60f7ba2a470cad1612f1d
Dylan Anderson <Dylan.Anderson@millerwelds.com>
2025-12-01 13:11:10 -0600

    Day 1 completed

---
+++ day1.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+
+int main()
+{
+    std::ifstream file("../assets/day1.txt");
+    std::vector<int> numbers;
+    std::string line;
+
+    if (!file.is_open())
+    {
+        std::cerr << "Error opening file" << std::endl;
+        return 1;
+    }
+
+    while (std::getline(file, line))
+    {
+        if (!line.empty())
+        {
+            char direction = line[0];
+            std::string number_str = line.substr(1, line.size() - 1);
+            int number = std::stoi(number_str);
+
+            if (direction == 'L')
+            {
+                numbers.push_back(-number);
+            }
+            else if (direction == 'R')
+            {
+                numbers.push_back(number);
+            }
+        }
+    }
+
+    file.close();
+
+    int position = 50;
+    int answer = 0;
+    for (int number : numbers)
+    {
+        int adjustments = std::abs(number);
+        int adjustment = (number > 0) ? 1 : -1;
+
+        while (adjustments > 0)
+        {
+            if (((position += adjustment) % 100) == 0)
+            {
+                answer++;
+            }
+
+            adjustments--;
+        }
+    }
+
+    std::cout << "Answer: " << answer << std::endl;
+
+    return 0;
+}
